beff91aa3d24a3604e6fd4ec772847734dcbc1fe
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2023-07-23 13:58:36 +0300

    Add Ink, Ink Flat & Sharp brushes & rename tempaltes

--- extra/get.fish
+++ extra/get.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 set PROJECT_DIR (git rev-parse --show-toplevel)
-set KRITA_RESOURCES_DIR ~/.local/share/krita
+set KRITA_RESOURCES_DIR ~/Sync/krita
 set KRITA_RESOURCE_DIRS (ls {$PROJECT_DIR}/krita)
 set SUFFIX "RazcoreRad"
 
 for dir in {$KRITA_RESOURCE_DIRS}
-  rm -f {$PROJECT_DIR}/krita/{$dir}/*
-  cp {$KRITA_RESOURCES_DIR}/{$dir}/*{$SUFFIX}* {$PROJECT_DIR}/krita/{$dir}
+  rm -fr {$PROJECT_DIR}/krita/{$dir}/*
+  cp -r {$KRITA_RESOURCES_DIR}/{$dir}/*{$SUFFIX}* {$PROJECT_DIR}/krita/{$dir}
 end
